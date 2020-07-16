063cd5ab1a9236d6871ab92dc24cbd0bc4e5beef
DBenson1017 <devinbenson@gmail.com>
2020-07-15 21:00:11 -0400

    not done

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,41 @@
+require "pry"
+
 class Owner
-  # code goes here
+  @@all = []
+  attr_reader :name, :species
+  def initialzie
+    @name = name 
+    @species = human
+    @@all << @name
+
+  end 
+
+  def say_species 
+    "I am a human."
+  end 
+
+  def name 
+    @name 
+  end 
+
+  def species 
+    @species 
+  end 
+
+  def all
+    #returns all instances of Owner that have been created
+    @@all 
+  end 
+
+def count
+  @@all.count
+end 
+
+def reset_all
+  @@all = []
+end 
+
+
+  binding.pry
+
 end
